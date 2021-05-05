95b2c64c40818d14394fb8ff2d5b6c13e80c08da
Fionnie Pollack <76826762+fionniepollack@users.noreply.github.com>
2021-05-05 17:06:07 -0400

    Create 206.py

---
+++ 206.py
@@ -0,0 +1,29 @@
+# 206. Reverse Linked List
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+
+        node = head
+        previous = None
+
+        # Case for empty linked list
+        if node == None:
+            return node
+                
+        while True:
+
+            if node.next == None:
+                node.next = previous
+                break
+
+            next = node.next
+            node.next = previous
+            previous = node
+            node = next
+
+        return node
